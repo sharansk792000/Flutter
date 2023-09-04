a8ea764f99e6bc8350a3846832886c1a60a5ca21
Kindalkar <sharansk792000@gmail.com>
2023-09-04 19:53:18 +0530

    update

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,22 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
+    MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.blue,
-        body: Center(
-          child: Text('Hi bro'),
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(colors: [
+              Color.fromARGB(255, 166, 15, 242),
+              Color.fromARGB(172, 68, 185, 239),
+            ], begin: Alignment.topLeft, end: Alignment.bottomRight),
+          ),
+          child: const Center(
+            child: Text(
+              'Hi bro',
+              style: TextStyle(color: Colors.white, fontSize: 22),
+            ),
+          ),
         ),
       ),
     ),
